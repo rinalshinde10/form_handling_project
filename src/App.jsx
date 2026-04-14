5f916ca01be93c50f559eebddad1d83b9d592dc0
rinalshinde10 <rnlshnd@gmail.com>
2026-04-14 13:29:37 +0530

    add basic code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
+
 function App() {
-  return <div>App</div>;
+  const [name, setName] = useState("");
+
+
+
+  return ( <div>
+    <h1>Form Handling</h1>
+    <input type="text" placeholder="Enter Your Name:"  onChange={(e) => { setName(e.target.value);} }  
+    value={name}/>
+    <br/> <br/>
+    <button type="button">Save</button>
+    <br/><br/>
+    <button type="button" onClick={() => { setName("");}}>Reset</button>
+    <h1>{name}</h1>
+  </div>);
 
 }
 export default App;
